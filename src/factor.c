986c98fe835c4b3335c351dec05374d35c3a884b
Joshua <joshuareedbarney@gmail.com>
2020-01-15 13:35:47 -0700

    Initial Commit

---
+++ src/factor.c
@@ -0,0 +1,16 @@
+/*
+	Find the prime factorization of a number.
+	Command line utility.
+	Josh Barney
+	joshuareedbarney@gmail.com
+	January 15th 2020
+*/
+
+include <stdio.h>
+
+int main(int argc, char * argv[]) {
+	
+	
+	return 0;
+}
+
