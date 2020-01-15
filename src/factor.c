399d39d6b3e6b89f3f4ac50f1e97054a5aff16be
Joshua <joshuareedbarney@gmail.com>
2020-01-15 14:20:03 -0700

    Figured out how to convert command line argument to int

--- src/factor.c
+++ src/factor.c
@@ -6,10 +6,19 @@
 	January 15th 2020
 */
 
-include <stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char * argv[]) {
-	
+	if (argc < 2) {
+		//
+		return 0;
+	}
+
+	int n = atoi(argv[1]);
+	printf(argv[1]);
+	printf("\n");
+	printf("%d", n);
 	
 	return 0;
 }
