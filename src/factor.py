a3187e250660ea1158aefb9b284f7d963a77570b
Joshua <joshuareedbarney@gmail.com>
2020-01-15 20:43:11 -0700

    Made python version of the program

---
+++ src/factor.py
@@ -0,0 +1,53 @@
+
+# Find the prime factorization of a number.
+# Command line utility.
+# Josh Barney
+# joshuareedbarney@gmail.com
+# January 15th 2020
+
+import math
+import sys
+
+def factor(n):
+    if n==0:
+        return
+    index = 0
+    sqrtOfN = math.sqrt(n) + 1
+    arr = []
+
+    while n != 1:
+        i = 2
+        while n % i != 0:
+            if i > sqrtOfN:
+                arr.append(int(n))
+                return arr
+            i += 1
+        arr.append(int(i))
+        n /= i
+        sqrtOfN = math.sqrt(n) + 1
+    
+    return arr
+    
+def main():
+    if len(sys.argv) < 2:
+        print("Please provide a number to factor")
+        return
+
+    n = int(sys.argv[1])
+    
+    if n <= 0:
+        print(f'{n}: \n')
+        return
+
+    arr = sorted(factor(n))
+
+    print(n, ':', end='', sep='')
+
+    for item in arr:
+        print(' ', item, end='', sep='')
+    
+    print()
+
+
+if __name__=='__main__':
+    main()
